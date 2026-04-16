16dd5a8bcdabdeff0f8acb02a64810babfbb46ff
k3ssily <cursoprozzz@gmail.com>
2026-04-15 21:32:01 -0300

    Add files via upload

---
+++ 4.0-°23.java
@@ -0,0 +1,12 @@
+public class Animal {
+
+    private String nome;
+
+    public Animal(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+}
